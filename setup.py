e87d64e32dfe93d2b12daf92c7fcd1ca4628fe76
Manoj Kandlikar <manoj.kandlikar@gmail.com>
2021-10-01 12:28:10 -0700

    fc

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='toolbox',
-    version='0.0.3',
-    author='Mike Huls',
-    author_email='mike_huls@hotmail.com',
-    description='Testing installation of Package',
+    name='quantwork',
+    version='0.0.1',
+    author='Manoj Kandlikar',
+    author_email='manoj.kandlikar@gmail.com',
+    description='Quant work',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/Muls/toolbox',
+    url='https://github.com/mankan1/quantwork.git',
     project_urls = {
-        "Bug Tracker": "https://github.com/Muls/toolbox/issues"
+        "Bug Tracker": "https://github.com/mankan1/quantwork/issues"
     },
     license='MIT',
-    packages=['toolbox'],
+    packages=['quantwork'],
     install_requires=['requests'],
 )
--- toolbox/__init__.py
+++
@@ -1,7 +0,0 @@
-# This is so that you can import ppack or import average from ppack
-# in stead of from ppack.functions import average
-
-from .decorators import singleton
-from .functions import listChunker, report, weirdCase
-
-
